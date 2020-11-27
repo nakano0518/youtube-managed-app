6ed1af905721172d7dd26f7df2113de26d289094
nakano0518 <m15sc018.prog@gmail.com>
2020-11-27 17:35:56 +0900

    get youtube movie from API

---
+++ frontend/store/index.js
@@ -0,0 +1,28 @@
+//アクションの定義
+import {createRequestClient} from '~/store/request-client';
+
+export const state = () => ({
+    items: [],
+    meta: {},
+})
+
+export const actions = {
+    async fetchPopularVideos({commit}, payload) {
+        const client = createRequestClient(this.$axios)
+        const res = await client.get(payload.uri, payload.params)
+        commit("mutatePopularVideos", res)
+    },
+}
+
+export const mutations = {
+    mutatePopularVideos(state, payload) {
+        state.items = payload.items ? state.items.concat(payload.items) : []
+        state.meta = payload
+    },
+}
+
+export const getters = { //Vueコンポーネントでstateを参照するためのgetter
+    getPopularVideos(state) {
+        return state.items 
+    }
+}
