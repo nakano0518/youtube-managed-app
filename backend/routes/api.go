e3ef556b8965e1ec924d9b0c49d607b0a0575853
nakano0518 <m15sc018.prog@gmail.com>
2020-11-28 14:53:31 +0900

    related movie API route

--- backend/routes/api.go
+++ backend/routes/api.go
@@ -11,5 +11,6 @@ func Init(e *echo.Echo) {
 	{
 		g.GET("/popular", api.FetchMostPopularVideos()) //endpoint("/api/popular")
 		g.GET("/video/:id", api.GetVideo())
+		g.GET("/related/:id", api.FetchRelatedVideos())
 	}
 }
---
+++ backend/web/api/fetch_related_videos.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
+	"github.com/valyala/fasthttp"
+	"google.golang.org/api/youtube/v3"
+)
+
+func FetchRelatedVideos() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		yts := c.Get("yts").(*youtube.Service)
+		videoId := c.Param("id")
+
+		call := yts.Search.
+			List([]string{"id", "snippet"}).
+			RelatedToVideoId(videoId).
+			Type("video"). //RelatedToVideoIdをしてする際この記述必要(要ドキュメント参照)
+			MaxResults(3)
+		res, err := call.Do()
+		if err != nil {
+			logrus.Fatalf("Error calling YouTube API: %v", err)
+		}
+
+		return c.JSON(fasthttp.StatusOK, res)
+	}
+}
