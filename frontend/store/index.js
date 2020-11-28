025250a2ffe9dd1b6dd3791eaadcc057940a9391
nakano0518 <m15sc018.prog@gmail.com>
2020-11-28 12:53:50 +0900

    youtube movie show page

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -3,6 +3,7 @@ import {createRequestClient} from '~/store/request-client';
 
 export const state = () => ({
     items: [],
+    item: {},
     meta: {},
 })
 
@@ -12,6 +13,14 @@ export const actions = {
         const res = await client.get(payload.uri, payload.params)
         commit("mutatePopularVideos", res)
     },
+    async findVideo({commit}, payload) {
+        const client = createRequestClient(this.$axios)
+        const res = await client.get(payload.uri)
+        const params = {
+            ...res.video_list,
+        }
+        commit('mutateVideo', params)
+    },
 }
 
 export const mutations = {
@@ -19,6 +28,10 @@ export const mutations = {
         state.items = payload.items ? state.items.concat(payload.items) : []
         state.meta = payload
     },
+    mutateVideo(state, payload) {
+        const params = (payload.items && payload.items.length > 0) ? payload.items[0] : {}
+        state.item = params
+    }
 }
 
 export const getters = { //Vueコンポーネントでstateを参照するためのgetter
@@ -27,5 +40,8 @@ export const getters = { //Vueコンポーネントでstateを参照するため
     },
     getMeta(state) {
         return state.meta
+    },
+    getVideo(state) {
+        return state.item
     },
 }
