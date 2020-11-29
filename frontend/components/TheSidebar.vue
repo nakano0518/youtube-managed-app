8b279d7efd7953487e567e2e3b8332dfe168c685
nakano0518 <m15sc018.prog@gmail.com>
2020-11-29 11:28:16 +0900

    favorite index display

--- frontend/components/TheSidebar.vue
+++ frontend/components/TheSidebar.vue
@@ -1,32 +1,34 @@
 <template>
+  <div class="column is-2">
     <div class="section">
-        <aside class="menu">
-            <ul class="menu-list">
-                <li>
-                    <nuxt-link
-                    to="/"
-                    >
-                    ホーム
-                    </nuxt-link> 
-                </li>
-            </ul>
-        </aside>
+      <aside class="menu">
+        <ul class="menu-list">
+          <li>
+            <nuxt-link
+                to="/"
+            >
+              ホーム
+            </nuxt-link>
+          </li>
+          <li v-if="isLoggedIn">
+            <nuxt-link
+                to="/favorite"
+            >
+              お気に入り
+            </nuxt-link>
+          </li>
+        </ul>
+      </aside>
     </div>
-</template>   
+  </div>
+</template>
 
-<style scoped>
-    .section {
-        margin-top: 1rem;
+<script>
+  export default {
+    computed: {
+      isLoggedIn() {
+        return !!this.$store.getters.isLoggedIn
+      }
     }
-    a {
-        color: #333;
-        text-decoration: none;
-    }
-    a:hover {
-        color: #aaa;
-    }
-    li {
-        padding: 0;
-        list-style-type: none;
-    }
-</style>
+  }
+</script>
